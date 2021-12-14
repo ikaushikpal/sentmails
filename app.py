29d7877042c93c2d7cdb8e940a2eadfd0bf7883e
SupriyoDam <67065203+SupriyoDam@users.noreply.github.com>
2021-12-14 16:55:26 +0530

    email text generation place added

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from src.emailsender import EmailSender
 import os
 from PIL import Image
 
+accepted_file_extention = ['csv','xml']
 
 PWD = os.path.dirname(os.path.abspath(__file__))
 st.title("Email Service Clone")
 
-left_side, right_side = st.columns([2, 1])
+left_side, right_side = st.columns([1, 3])
 
 with left_side:
     path = os.path.join(PWD, 'static', 'pictures', 'email_background.png')
@@ -20,7 +21,7 @@ with right_side:
 
     # st.write('Email')
     id = st.text_input("Email ID")
-    pw = st.text_input('Password')
+    pw = st.text_input('Password',type='password')
     st.button('Login')
 
     if id=="admin" and pw == "root":
@@ -30,4 +31,17 @@ with right_side:
         validation = False
     
     if validation:
-        st.subheader('Login Successfull.')
+        st.write('Login Successfull.')
+        st.subheader("Email Editor:")
+        mail_sub = st.text_input('Subject')
+        body_type = st.selectbox('Message Body Type',('Plain Text','HTML'))
+        mail_body = st.text_area('Body')
+        
+        uploaded_file = st.file_uploader("Choose recipient file",type=accepted_file_extention)
+        if uploaded_file is not None:
+            bytes_data = uploaded_file.read()
+            st.write("filename:", uploaded_file.name)
+
+    # for validation failure
+    else:
+        st.write("Login Failed. Kindly try again.")
