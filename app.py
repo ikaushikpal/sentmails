1079867ff72085f546a5f8f4d76c514d8478359d
SupriyoDam <67065203+SupriyoDam@users.noreply.github.com>
2021-12-14 15:31:59 +0530

    updated app.py

--- app.py
+++ app.py
@@ -2,10 +2,27 @@ import streamlit as st
 
 st.title("Email Service Clone")
 
-c1, c2 = st.columns(2,1)
+c1, c2 = st.columns([1,1])
 
 with c1:
-    st.header("s")
+    # 
+    pass
 
 with c2:
     st.subheader("Login Page")
+    st.write()
+
+    # st.write('Email')
+    id = st.text_input("Email ID")
+    pw = st.text_input('Password')
+    st.button('Login')
+
+    if id=="admin" and pw == "root":
+        validation = True
+
+    else:
+        validation = False
+    
+    if validation:
+        with c2:
+            st.subheader('Successfull.')
