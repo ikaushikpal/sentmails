129afc5ae451ce0b7e5c9b2ac10fa6ffd74340f7
Laptop@ikaushikpal <iamkaushik@Laptop.com>
2021-12-14 15:28:30 +0530

    added picture support

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import streamlit as st
+from src.loadserver import LoadServer
+from src.emailsender import EmailSender
+import os
+from PIL import Image
 
+
+PWD = os.path.dirname(os.path.abspath(__file__))
 st.title("Email Service Clone")
 
-c1, c2 = st.columns(2,1)
+left_side, right_side = st.columns([2, 1])
 
-with c1:
-    st.header("s")
+with left_side:
+    path = os.path.join(PWD, 'static', 'pictures', 'email_background.png')
+    image = Image.open(path)
+    st.image(image)
 
-with c2:
+with right_side:
     st.subheader("Login Page")
